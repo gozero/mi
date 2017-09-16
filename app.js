5f3f982d296958748af0f95041b1bcd6db6d7879
guyimin <1205738549@qq.com>
2017-09-16 12:10:51 +0800

    feat(header): add nav and search form

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
 	}, function() {
 		$(this).removeClass('topbar-cart-active').find('.cart-menu').slideUp(200)
 	})
+
+	// header 搜索框
+	$('.header .search-form .search-text').focus(function() {
+		$(this).css('border-color', '#ff6700').next().css('border-color', '#ff6700').next().fadeOut(200).next().slideDown(200)
+	}).blur(function() {
+		$(this).css('border-color', '#e0e0e0').next().css('border-color', '#e0e0e0').next().fadeIn(200).next().slideUp(200)
+	})
 })
