02b9190bf5df4c454b62e6820827ca01ffea1910
guyimin <1205738549@qq.com>
2017-09-16 12:01:32 +0800

    feat(topbar): add topbar

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+	// topbar 购物车
+	$('.topbar .topbar-cart').hover(function() {
+		$(this).addClass('topbar-cart-active').find('.cart-menu').slideDown(200)
+	}, function() {
+		$(this).removeClass('topbar-cart-active').find('.cart-menu').slideUp(200)
+	})
 })
