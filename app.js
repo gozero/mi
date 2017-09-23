8d87acf56c827d7d417cc7a5aa0e16d4a50c54ac
guyimin <1205738549@qq.com>
2017-09-23 17:43:17 +0800

    feat(home-container): add carousel and goods show

--- app.js
+++ app.js
@@ -26,4 +26,54 @@ $(document).ready(function() {
     }).blur(function() {
         $(this).css('border-color', '#e0e0e0').next().css('border-color', '#e0e0e0').next().fadeIn(200).next().slideUp(200)
     })
+
+    // home-container 轮播图, 定时器
+    var carouselTimer = setInterval(function() {
+        var $homeGoodsSlider = $('.home-container .home-goods-slider')
+        var $slideItems = $homeGoodsSlider.find('.carousel-viewport .slide-item')
+        var $pagerItems = $homeGoodsSlider.find('.carousel-controls-pager .pager-item')
+        var activatedIndex = $homeGoodsSlider.find('.carousel-viewport .slide-item-active').index()
+        var nextIndex = 0
+        if (activatedIndex >= ($slideItems.length - 1)) {
+            nextIndex = 0
+        } else {
+            nextIndex = activatedIndex + 1
+        }
+        $slideItems.removeClass('slide-item-active').hide().eq(nextIndex).addClass('slide-item-active').fadeIn(1000)
+        $pagerItems.removeClass('pager-item-active').eq(nextIndex).addClass('pager-item-active')
+    }, 3000)
+
+    // home-container 轮播图, 左右箭头点击事件
+    $('.home-container .carousel-controls-direction').on('click', 'a', function(event) {
+        clearInterval(carouselTimer)
+        var direction = $(this).attr('class')
+        var $homeGoodsSlider = $(this).parents('.home-goods-slider')
+        var $slideItems = $homeGoodsSlider.find('.carousel-viewport .slide-item')
+        var $activatedItem = $homeGoodsSlider.find('.carousel-viewport .slide-item-active')
+        var $siblingItem = undefined
+        if (direction === 'prev') {
+            $siblingItem = $activatedItem.prev('.slide-item')
+            if ($siblingItem.length === 0) {
+                $siblingItem = $slideItems.eq($slideItems.length - 1)
+            }
+        } else {
+            $siblingItem = $activatedItem.next('.slide-item')
+            if ($siblingItem.length === 0) {
+                $siblingItem = $slideItems.eq(0)
+            }
+        }
+        $activatedItem.removeClass('slide-item-active').hide()
+        $siblingItem.addClass('slide-item-active').fadeIn(1000)
+        $homeGoodsSlider.find('.carousel-controls-pager .pager-item').removeClass('pager-item-active').eq($siblingItem.index()).addClass('pager-item-active')
+    })
+
+    // home-container 轮播图, 小圆点点击事件
+    $('.home-container .carousel-controls-pager').on('click', 'a', function(event) {
+        if (!$(this).hasClass('pager-item-active')) {
+            clearInterval(carouselTimer)
+            var index = $(this).attr('data-index')
+            $(this).parents('.carousel-controls-pager').find('.pager-item').removeClass('pager-item-active').eq(index).addClass('pager-item-active')
+            $(this).parents('.home-goods-slider').find('.carousel-viewport .slide-item').removeClass('slide-item-active').hide().eq(index).addClass('slide-item-active').fadeIn(1000)
+        }
+    })
 })
